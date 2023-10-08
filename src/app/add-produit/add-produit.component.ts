a618f8b721998aa3bedbb4c454df7b73f15bb2b1
hmandimalak <malakhmandi02@gmail.com>
2023-10-08 17:30:27 +0100

    tp2

--- src/app/add-produit/add-produit.component.ts
+++ src/app/add-produit/add-produit.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
-
+import { Produit } from '../model/produit.model';
+import { ProduitService } from '../services/produit.service';
 @Component({
   selector: 'app-add-produit',
   templateUrl: './add-produit.component.html',
   styleUrls: ['./add-produit.component.css']
 })
 export class AddProduitComponent {
+  newProduit = new Produit();
+  message : string | undefined;
+  constructor(private produitService: ProduitService ) {}
+  addProduit(){
+    //console.log(this.newProduit);
+    this.produitService.ajouterProduit(this.newProduit);
+    this.message = "produit" + this.newProduit.nomProduit +"ajouter avec succés"
+  }
 
 }
